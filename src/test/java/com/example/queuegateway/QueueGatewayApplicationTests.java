563a2da451bd7878b5b77e4599c0fd737dc7f709
jeong_jeagong <jeong_jeagong@cocone.co.jp>
2021-08-08 18:57:16 +0900

    Initial commit

---
+++ src/test/java/com/example/queuegateway/QueueGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.queuegateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueueGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
